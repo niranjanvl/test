fdeeb393317dce56d28d51ab8fbc2deb919b63a1
babji <venkatababji@gmail.com>
2021-06-03 20:07:59 +0530

    first cut

---
+++ py/misc/min_max.py
@@ -0,0 +1,44 @@
+"""
+Given a list of numbers,
+    find the maximum number
+      and the minimum number 
+      and the count of negative numbers
+      and the count of positive numbers
+
+
+> max_min.py 3 4 5 2 30 -1 6 7 8 -2 
+   max : 30
+   min : -2
+   -ve : 2
+   +ve : 8
+
+> max_min.py  31 4 5 2 30 6 7 -8 100
+   max : 100
+   min : -8
+   -ve : 1
+   +ve : 8
+
+"""
+
+import sys
+
+if(len(sys.argv) < 2):
+    print("Usage :\n\t min_max.py <list of numbers>")
+    exit(0)
+           
+num_list = [int(x) for x in sys.argv[1:]]
+print(num_list)
+
+max = num_list[0]
+min = num_list[0]
+for n in num_list:
+    if max < n:
+        max = n
+    elif min > n:
+        min = n
+
+print("max : {0}".format(max))
+print("min : {0}".format(min))
+
+
+
