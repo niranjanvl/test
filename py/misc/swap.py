f05817f67428f053f644987117b775004254469a
babji <venkatababji@gmail.com>
2021-06-04 20:09:27 +0530

    swap assignement

---
+++ py/misc/swap.py
@@ -0,0 +1,28 @@
+'''
+Given two variable swap their values
+i.e.,
+    n = 10
+    m = 12
+    print("m : {0}, n : {1}".format(m,n))
+        --> m : 10, n : 12
+    #-    
+    #TODO : Swap values of m and n
+    #-
+    print("m : {0}, n : {1}".format(m,n))
+        --> m : 12, n : 10
+'''
+
+n = 10
+m = 12
+print("m : {0}, n : {1}".format(m,n))
+
+#-    
+#TODO : Swap values of m and n
+#-
+
+#-- begin your code - cant use constats like 10, 12.
+#   only variables and logic is permitted
+
+#-- end your code
+
+print("m : {0}, n : {1}".format(m,n))
