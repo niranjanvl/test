9232f401022d56f6c013ab3c5ae488da6c0b3fcd
babji <venkatababji@gmail.com>
2021-06-07 19:22:11 +0530

    implemented to the TODO

--- py/misc/swap.py
+++ py/misc/swap.py
@@ -12,17 +12,25 @@ i.e.,
         --> m : 12, n : 10
 '''
 
-n = 10
-m = 12
+n = 12
+m = 10
 print("m : {0}, n : {1}".format(m,n))
 
 #-    
-#TODO : Swap values of m and n
+# Swap values of m and n
 #-
 
-#-- begin your code - cant use constats like 10, 12.
+#-- begin your code - cant use constants like 10, 12.
 #   only variables and logic is permitted
-
-#-- end your code
+if m != n:
+    temp = n
+    n = m
+    m = temp
 
 print("m : {0}, n : {1}".format(m,n))
+
+#python supports the below
+if m != n:
+    m, n = n, m
+print("m : {0}, n : {1}".format(m,n))
+
