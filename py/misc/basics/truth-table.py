ec2b44f0a24a34de4a8c4a9e7b0f7b6cb1fb55ea
babji <venkatababji@gmail.com>
2021-09-17 19:27:01 +0530

    filled all the operators

--- py/misc/basics/truth-table.py
+++ py/misc/basics/truth-table.py
@@ -47,16 +47,28 @@ if(operator == 'and'):
     print("F F : {0}".format(TF(False and False)))
 
 elif(operator == 'or'):
-    print("TODO")
+    print("T T : {0}".format(TF(True or True)))
+    print("T F : {0}".format(TF(True or False)))
+    print("F T : {0}".format(TF(False or True)))
+    print("F F : {0}".format(TF(False or False)))
 
 elif(operator == 'xor'):
-    print("TODO")
+    print("T T : {0}".format(TF(True ^ True)))
+    print("T F : {0}".format(TF(True ^ False)))
+    print("F T : {0}".format(TF(False ^ True)))
+    print("F F : {0}".format(TF(False ^ False)))
 
 elif(operator == 'nor'):
-    print("TODO")
+    print("T T : {0}".format(TF(not(True or True))))
+    print("T F : {0}".format(TF(not(True or False))))
+    print("F T : {0}".format(TF(not(False or True))))
+    print("F F : {0}".format(TF(not(False or False))))
 
 elif(operator == 'nand'):
-    print("TODO")
+    print("T T : {0}".format(TF(not(True and True))))
+    print("T F : {0}".format(TF(not(True and False))))
+    print("F T : {0}".format(TF(not(False and True))))
+    print("F F : {0}".format(TF(not(False and False))))
 
 else:
     print("Invalid operator")
