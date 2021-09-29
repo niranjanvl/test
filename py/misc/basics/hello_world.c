a232a198352b8185c830c8d05e6a006cc7d0d761
babji <venkatababji@gmail.com>
2021-09-29 19:38:01 +0530

    c code

---
+++ py/misc/basics/hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    printf("Hello World\n");
+    printf("%d\n", 100+130);
+    printf("%d\n", 100*100);
+
+    int x;
+    x = 1;
+    printf("%d\n", x);
+    x = "hi";
+    printf("%d\n", x);
+    return 0;
+}
---
+++ py/misc/basics/hello_world.s
@@ -0,0 +1,46 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$32, %rsp
+	movl	$0, -4(%rbp)
+	movl	%edi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	leaq	L_.str.1(%rip), %rdi
+	movl	$230, %esi
+	movl	%eax, -20(%rbp)         ## 4-byte Spill
+	movb	$0, %al
+	callq	_printf
+	leaq	L_.str.1(%rip), %rdi
+	movl	$10000, %esi            ## imm = 0x2710
+	movl	%eax, -24(%rbp)         ## 4-byte Spill
+	movb	$0, %al
+	callq	_printf
+	xorl	%esi, %esi
+	movl	%eax, -28(%rbp)         ## 4-byte Spill
+	movl	%esi, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"Hello World\n"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"%d\n"
+
+
+.subsections_via_symbols
